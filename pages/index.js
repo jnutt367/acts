52239760f47ce1287fcbf572408aa58e3610e7c3
Jason Nutt <jnutt367@gmail.com>
2022-07-05 19:46:02 -0400

    commit for index in place of donate

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,11 @@ export default function Home() {
             />
 
 <a className={styles.code} href="https://the-holy-bible.vercel.app">HOME</a>
+            <a href="https://www.linkedin.com/in/jnuttlovedisciple/">
             <button className={styles.code}>
-                DONATE
+                INDEX
             </button>
+            </a>
         </div>
       <main className={styles.main}>
     
