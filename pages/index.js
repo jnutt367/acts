adc643321c96b841d60b292d6e7bce400b992a5e
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-29 11:54:52 -0500

    Update index.js
      <div className={styles.image}>

--- pages/index.js
+++ pages/index.js
@@ -21,13 +21,15 @@ export default function Home() {
        
         </h1>
         <div className={styles.hero}>
-       <Image 
-                                src="/ascension-day-illustration-with-jesus-christ_23-2148545877 (1).webp"
-                                alt="Jesus teaching"
-                                height={900}
-                                width={2000}
-                                priority
-                              />
+                <div className={styles.image}>
+           <Image 
+                                    src="/ascension-day-illustration-with-jesus-christ_23-2148545877 (1).webp"
+                                    alt="Jesus teaching"
+                                    height={900}
+                                    width={2000}
+                                    priority
+                                  />
+                </div>                      
             <div className={styles.nav}>              
         <a href="https://the-holy-bible.vercel.app/">
             <button className={styles.button}> HOME </button>
