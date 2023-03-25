17c054192f77b34b2e63bc1f42f1d1d283bdb1b4
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-03-25 10:16:56 -0400

    Update index.js
    jesus-sky (1).gif

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
         </h1>
                 <div className={styles.image}>
            <Image 
-                                    src="/jesus-dios.gif"
+                                    src="/jesus-sky (1).gif"
                                     alt="Jesus teaching"
                                     height={900}
                                     width={2000}
