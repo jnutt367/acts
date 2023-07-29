03341e713ab589cb10a3eba237b19efc62ea231f
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-29 11:11:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1127,6 +1127,203 @@ and turn, and I would heal them.’[a]
  
       </main>
 
+<main id="philemon" className={styles.main}>
+       
+        <div id="first_thessalonians" className={styles.hero}>
+                <h2 className={styles.holy}>THE WORD</h2>
+                <h1 className={styles.bible}>
+                    <a href="https://the-word-of-god.vercel.app/" target="_blank">OF GOD</a>
+                </h1>
+        <h1 className={styles.title}>
+       HEBREWS
+        </h1>
+          <div className={styles.image}>
+                           <Image 
+                                                    src="/philemon.png"
+                                                    alt="hero image for Thessalonians"
+                                                    height={900}
+                                                    width={2000}
+                                                    priority
+                                                  />
+                          
+
+                        </div>
+    <p className={styles.parables}>
+
+         Hebrwes is written anonymously & addresses an audience familiar to the old covenant of the God of Israel. It elevates Jesus as superior to any one or anything else before Him.
+</p>                                                      
+                  <div className={styles.nav2}>           
+      <a href="https://luke-three.vercel.app/#philemon">
+             <Image 
+                                src="/previous.webp"
+                                alt="prev"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+       </a>
+
+        
+      <a href="https://cash.app/$JNutt367">
+              <Image className={styles.arrow}
+                                src="/donations_please.webp"
+                                alt="donations_please.webp"
+                                height={75}
+                                width={90}
+                                priority
+                              /> 
+       </a>
+         <a href="https://acts-zeta.vercel.app/#hebrews">
+           <Image 
+                                src="/next.webp"
+                                alt="next"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         
+               </div>
+               <div className={styles.iframe}> 
+<iframe width="100%" height="315" src="https://www.youtube.com/embed/1fNWTZZwgbs?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                    
+                                  </div>
+                                  
+                              
+        <div className={styles.grid2}>
+          <div className={styles.card2}>
+           <h2  className={styles.chapter2}>Hebrews Chapter I</h2>
+            <p> 
+
+God’s Final Word: His Son
+1 In the past God spoke to our ancestors through the prophets at many times and in various ways, 2 but in these last days he has spoken to us by his Son, whom he appointed heir of all things, and through whom also he made the universe. 3 The Son is the radiance of God’s glory and the exact representation of his being, sustaining all things by his powerful word. After he had provided purification for sins, he sat down at the right hand of the Majesty in heaven. 4 So he became as much superior to the angels as the name he has inherited is superior to theirs.
+
+The Son Superior to Angels
+5 For to which of the angels did God ever say,
+
+“You are my Son;
+    today I have become your Father”[a]?
+
+Or again,
+
+“I will be his Father,
+    and he will be my Son”[b]?
+
+6 And again, when God brings his firstborn into the world, he says,
+
+“Let all God’s angels worship him.”[c]
+
+7 In speaking of the angels he says,
+
+“He makes his angels spirits,
+    and his servants flames of fire.”[d]
+
+8 But about the Son he says,
+
+“Your throne, O God, will last for ever and ever;
+    a scepter of justice will be the scepter of your kingdom.
+9 You have loved righteousness and hated wickedness;
+    therefore God, your God, has set you above your companions
+    by anointing you with the oil of joy.”[e]
+
+10 He also says,
+
+“In the beginning, Lord, you laid the foundations of the earth,
+    and the heavens are the work of your hands.
+11 They will perish, but you remain;
+    they will all wear out like a garment.
+12 You will roll them up like a robe;
+    like a garment they will be changed.
+But you remain the same,
+    and your years will never end.”[f]
+
+13 To which of the angels did God ever say,
+
+“Sit at my right hand
+    until I make your enemies
+    a footstool for your feet”[g]?
+
+14 Are not all angels ministering spirits sent to serve those who will inherit salvation?
+                          
+  </p>
+          </div>
+
+          <div className={styles.card2}>
+            <h2 className={styles.chapter2}>Hebrews Chapter II</h2>
+            <p> 
+
+Warning to Pay Attention
+2 We must pay the most careful attention, therefore, to what we have heard, so that we do not drift away. 2 For since the message spoken through angels was binding, and every violation and disobedience received its just punishment, 3 how shall we escape if we ignore so great a salvation? This salvation, which was first announced by the Lord, was confirmed to us by those who heard him. 4 God also testified to it by signs, wonders and various miracles, and by gifts of the Holy Spirit distributed according to his will.
+
+Jesus Made Fully Human
+5 It is not to angels that he has subjected the world to come, about which we are speaking. 6 But there is a place where someone has testified:
+
+“What is mankind that you are mindful of them,
+    a son of man that you care for him?
+7 You made them a little[a] lower than the angels;
+    you crowned them with glory and honor
+8     and put everything under their feet.”[b][c]
+
+In putting everything under them,[d] God left nothing that is not subject to them.[e] Yet at present we do not see everything subject to them.[f] 9 But we do see Jesus, who was made lower than the angels for a little while, now crowned with glory and honor because he suffered death, so that by the grace of God he might taste death for everyone.
+
+10 In bringing many sons and daughters to glory, it was fitting that God, for whom and through whom everything exists, should make the pioneer of their salvation perfect through what he suffered. 11 Both the one who makes people holy and those who are made holy are of the same family. So Jesus is not ashamed to call them brothers and sisters.[g] 12 He says,
+
+“I will declare your name to my brothers and sisters;
+    in the assembly I will sing your praises.”[h]
+
+13 And again,
+
+“I will put my trust in him.”[i]
+
+And again he says,
+
+“Here am I, and the children God has given me.”[j]
+
+14 Since the children have flesh and blood, he too shared in their humanity so that by his death he might break the power of him who holds the power of death—that is, the devil— 15 and free those who all their lives were held in slavery by their fear of death. 16 For surely it is not angels he helps, but Abraham’s descendants. 17 For this reason he had to be made like them,[k] fully human in every way, in order that he might become a merciful and faithful high priest in service to God, and that he might make atonement for the sins of the people. 18 Because he himself suffered when he was tempted, he is able to help those who are being tempted.
+
+      
+  </p>
+          </div>
+
+        
+       
+          <div className={styles.card2}
+          >
+            <h2 className={styles.chapter2}>!Explaination of Layout!</h2>
+            <p>
+            I have appended 1 Thessalonians to the end of 1 corinthians...
+            I have appended 2 Thessalonians to the end of 2 corinthians...
+            I have appended 1 Timothy to the end of the gospel of Matthew...
+            I have appended 2 Timothy to the end of the gospel of Mark...
+            I have appended Titus to the end of the gospel of Luke...
+            I have appended Philemon to the end of the gospel of John...
+            I AM !!! CURRENTLY APPENDING on chapter 2 !!! Hebrews to the end of the book of Acts ...
+            I will be appending James to the end of Romans ...
+            I will be appending Peter to the end of I Corinthians ... 
+            I will be appending 2 Peter to the end of II Corinthians ... 
+            I will be appending 1 John to the end of Galatians ... 
+            I will be appending 2 John to the end of Ephesians ... 
+            I will be appending 3 John to the end Philippians ... 
+            I will be appending Jude to the end ofColossians ... 
+                        More to come on the layout...!
+     </p>
+          </div>
+
+
+     
+
+        </div>
+</div>
+        <p className={styles.description}> 
+        <code className={styles.publisher}>
+        Coded, Published & Deployed by His servant: Jason Daniel Nutt 
+        </code>
+        </p>
+      </main>
+
+
+
+  
     
     </div>
   )
