0d0a91f02d332171656a4cfff2d2602c63b874f8
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-29 22:47:39 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1315,7 +1315,36 @@ And again he says,
                                                 16 Who were they who heard and rebelled? Were they not all those Moses led out of Egypt? 17 And with whom was he angry for forty years? Was it not with those who sinned, whose bodies perished in the wilderness? 18 And to whom did God swear that they would never enter his rest if not to those who disobeyed? 19 So we see that they were not able to enter, because of their unbelief.       
                                                           </p>
                       </div>  
+
+
+                       <div className={styles.card2}>
+                                             <h2  className={styles.chapter2}>Hebrews Chapter IV</h2>
+                                              <p> 
+                                                       <h4> A Sabbath-Rest for the People of God</h4>
+                                  4 Therefore, since the promise of entering his rest still stands, let us be careful that none of you be found to have fallen short of it. 2 For we also have had the good news proclaimed to us, just as they did; but the message they heard was of no value to them, because they did not share the faith of those who obeyed.[a] 3 Now we who have believed enter that rest, just as God has said,
+                                  
+                                  “So I declared on oath in my anger,
+                                      ‘They shall never enter my rest.’”[b]
+                                  
+                                  And yet his works have been finished since the creation of the world. 4 For somewhere he has spoken about the seventh day in these words: “On the seventh day God rested from all his works.”[c] 5 And again in the passage above he says, “They shall never enter my rest.”
+                                  
+                                  6 Therefore since it still remains for some to enter that rest, and since those who formerly had the good news proclaimed to them did not go in because of their disobedience, 7 God again set a certain day, calling it “Today.” This he did when a long time later he spoke through David, as in the passage already quoted:
+                                  
+                                  “Today, if you hear his voice,
+                                      do not harden your hearts.”[d]
+                                  
+                                  8 For if Joshua had given them rest, God would not have spoken later about another day. 9 There remains, then, a Sabbath-rest for the people of God; 10 for anyone who enters God’s rest also rests from their works,[e] just as God did from his. 11 Let us, therefore, make every effort to enter that rest, so that no one will perish by following their example of disobedience.
+                                  
+                                  12 For the word of God is alive and active. Sharper than any double-edged sword, it penetrates even to dividing soul and spirit, joints and marrow; it judges the thoughts and attitudes of the heart. 13 Nothing in all creation is hidden from God’s sight. Everything is uncovered and laid bare before the eyes of him to whom we must give account.
+                                  
+                                  Jesus the Great High Priest
+                                  14 Therefore, since we have a great high priest who has ascended into heaven,[f] Jesus the Son of God, let us hold firmly to the faith we profess. 15 For we do not have a high priest who is unable to empathize with our weaknesses, but we have one who has been tempted in every way, just as we are—yet he did not sin. 16 Let us then approach God’s throne of grace with confidence, so that we may receive mercy and find grace to help us in our time of need.
   
+                                        </p>
+                      </div>                          
+
+
+                                                  
   <div className={styles.card2}
           >
             <h2 className={styles.chapter2}>!Explaination of Layout!</h2>
